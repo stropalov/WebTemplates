7382ce38dc8ab98ab2df347e7407f218b3ce81fb
stropalov <stropalov@gmail.com>
2015-02-25 23:26:22 +0200

    Add toggle menu

--- LandingPagePortfolio/js/main.js
+++ LandingPagePortfolio/js/main.js
@@ -1,18 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	// set min-height to max window height
-	function setMinHeight(){
-		$(".main_head").css("min-height", $(window).height() );
+	function setMinHeight() {
+		$(".main_head").css("height", $(window).height());
 	};
 	setMinHeight();
 	// on resize action
-	$(window).resize(function(){
+	$(window).resize(function () {
 		setMinHeight();
 	});
-	
-	
-	$(window).load(function() { 
-	$(".loader_inner").fadeOut(); 
-	$(".loader").delay(500).fadeOut("slow"); 
-});
-	
+
+	$(window).load(function () {
+		$(".loader_inner").fadeOut();
+		$(".loader").delay(500).fadeOut("slow");
+
+		$(".sandwich, .menu_item").click(function () {
+			$(".sandwich").toggleClass("active");
+		});
+	});
+
+	$(".toggle_menu").click(function () {
+		if ($(".top_menu").is(":visible")) {
+			$(".top_menu").fadeOut(600);
+		} else {
+			$(".top_menu").fadeIn(600);
+		}
+	});
+
+
 });

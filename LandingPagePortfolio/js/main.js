ae2e5343ccbb34b77e18e71b51e5c11f34b8ff7a
stropalov <stropalov@gmail.com>
2015-02-26 07:37:47 +0200

    Add text block on header

--- LandingPagePortfolio/js/main.js
+++ LandingPagePortfolio/js/main.js
@@ -18,11 +18,19 @@ $(document).ready(function () {
 		});
 	});
 
+	
+	$(".top_menu ul a").click(function () {
+		$(".top_menu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+	}).append("<span>");
+	
 	$(".toggle_menu").click(function () {
 		if ($(".top_menu").is(":visible")) {
 			$(".top_menu").fadeOut(600);
+			$(".top_menu li a").removeClass("fadeInUp animated");
 		} else {
 			$(".top_menu").fadeIn(600);
+			$(".top_menu li a").addClass("fadeInUp animated");
 		}
 	});
 

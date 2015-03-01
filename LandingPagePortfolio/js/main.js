289330af015eb9b7ff49bc1b3594b73b32eb9a05
stropalov <stropalov@gmail.com>
2015-03-01 20:53:32 +0200

    Add content to contacts section

--- LandingPagePortfolio/js/main.js
+++ LandingPagePortfolio/js/main.js
@@ -10,25 +10,29 @@ $(document).ready(function () {
 	});
 
 	$("#portfolio_grid").mixItUp();
-	
-	$(".s_portfolio li").click(function(){
+
+	$(".s_portfolio li").click(function () {
 		$(".s_portfolio li").removeClass("active");
 		$(this).addClass("active");
 	});
-	
-	$(".top_text h1").animated("fadeInDown", "fadeOutUp");	
-	$(".top_text p, .section_header").animated("fadeInUp", "fadeOutDown");	
-	
-	$(".animation1").animated("flipInY", "fadeOutY");	
-	$(".animation2").animated("fadeInLeft", "fadeOutLeft");	
+
+	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
+	$(".top_text p, .section_header").animated("fadeInUp", "fadeOutDown");
+
+	$(".animation1").animated("flipInY", "fadeOutY");
+	$(".animation2").animated("fadeInLeft", "fadeOutLeft");
 	$(".animation3").animated("fadeInRight", "fadeOutRight");
-	
-	$(".left .resume_item").animated("fadeInLeft", "fadeOutDown");	
-	$(".right .resume_item").animated("fadeInRight", "fadeOutDown");	
-	
-	$(".popup").magnificPopup({type: "image"});
-	$(".popup_content").magnificPopup({type: "inline"});
-	
+
+	$(".left .resume_item").animated("fadeInLeft", "fadeOutDown");
+	$(".right .resume_item").animated("fadeInRight", "fadeOutDown");
+
+	$(".popup").magnificPopup({
+		type: "image"
+	});
+	$(".popup_content").magnificPopup({
+		type: "inline"
+	});
+
 	$(window).load(function () {
 		$(".loader_inner").fadeOut();
 		$(".loader").delay(500).fadeOut("slow");
@@ -37,22 +41,31 @@ $(document).ready(function () {
 			$(".sandwich").toggleClass("active");
 		});
 	});
-
+	
+	$(".toggle_menu").click(function () {
+		$(".sandwich").toggleClass("active");
+	});
 	
 	$(".top_menu ul a").click(function () {
 		$(".top_menu").fadeOut(600);
 		$(".sandwich").toggleClass("active");
+		$(".top_text").css("opacity", "1");
 	}).append("<span>");
-	
+
+
 	$(".toggle_menu").click(function () {
 		if ($(".top_menu").is(":visible")) {
+			$(".top_text").css("opacity", "1");
 			$(".top_menu").fadeOut(600);
 			$(".top_menu li a").removeClass("fadeInUp animated");
 		} else {
+			$(".top_text").css("opacity", ".1");
 			$(".top_menu").fadeIn(600);
 			$(".top_menu li a").addClass("fadeInUp animated");
 		}
 	});
 
+	$("input, select, textarea").not("[type=submit]").jqBootstrapValidation();
 
+	$(".top_menu ul a").mPageScroll2id();
 });

c1308ce426923970c1bddc38dfeb5c110690ade5
stropalov <stropalov@gmail.com>
2015-03-03 07:17:34 +0200

    Making WP theme

---
+++ LandingPagePortfolio/MyTheme/functions.php
@@ -0,0 +1,19 @@
+<?php
+
+show_admin_bar(false);
+
+function logo_widget_init() {
+	register_sidebar( array(
+		'name'          => 'Логотип SVG',
+		'id'            => 'logo',
+		'before_widget' => '',
+		'after_widget'  => '',
+		'before_title'  => '<span class="hidden">',
+		'after_title'   => '</span>',
+	) );
+}
+add_action( 'widgets_init', 'logo_widget_init' );
+
+require_once ( get_stylesheet_directory() . '/theme-options.php' );
+
+add_theme_support('post-thumbnails');

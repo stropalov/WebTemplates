0de2d196dce50839d3dbe8ca38edad9cf27595cd
stropalov <stropalov@gmail.com>
2015-03-07 21:37:20 +0200

    Add header

--- LandingPageCourses/js/main.js
+++ LandingPageCourses/js/main.js
@@ -10,9 +10,15 @@ $(document).ready(function () {
 	});
 
 
-	// tabs
-	$(".wrapper .tab").click(function () {
-		$(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
-		$(".tab_item").hide().eq($(this).index()).fadeIn()
+	// tabs for telephones
+	$(".top_phone .wrapper .tab").click(function () {
+		$(".top_phone .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".top_phone .tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");
+	
+	// tabs for extras
+	$(".tabs_header .wrapper .tab").click(function () {
+		$(".tabs_header .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".tabs_header .tab_item").hide().eq($(this).index()).fadeIn()
 	}).eq(0).addClass("active");
 });

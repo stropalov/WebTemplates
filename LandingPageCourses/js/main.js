b5e8cf1bad7ab276ba461d2861386edeb25c6d26
stropalov <stropalov@gmail.com>
2015-03-14 17:11:20 +0200

    Add animation, popup form

--- LandingPageCourses/js/main.js
+++ LandingPageCourses/js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+	
+	
+	$(".popup_c").magnificPopup();
+	
 	// parallax background
 	$.stellar({
 		responsive: true,
@@ -50,4 +54,35 @@ $(document).ready(function () {
 		$(".contacts_top .tab").removeClass("active").eq($(this).index()).addClass("active");
 		$(".s_contacts .tab_item").hide().eq($(this).index()).fadeIn()
 	}).eq(0).addClass("active");
+	
+	$("form").submit(function(e) {
+		var ths = $(this);
+		e.preventDefault;
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				var magnificPopup = $.magnificPopup.instance; 
+				magnificPopup.close();
+				ths.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+});
+
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+	$(".top_header").animated("fadeInDown", "fadeOut");
+	$(".tabs_header .wrapper").animated("flipInY", "fadeOut");
+	$(".profi_item").animated("fadeInRight", "fadeOut");
+	$(".s_pofi form").animated("zoomInRight", "fadeOut");
+	$(".s_back h3").animated("fadeInUp", "fadeOut");
+	$("section h2, footer h2, .contacts_top .tabs").animated("fadeInUp", "fadeOut");
 });

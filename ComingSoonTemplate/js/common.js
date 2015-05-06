cdf9cfb52ed33bd2f175accfb3dcadee4b8ce1ad
stropalov <stropalov@gmail.com>
2015-05-06 07:40:23 +0300

    Add header countdown

--- ComingSoonTemplate/js/common.js
+++ ComingSoonTemplate/js/common.js
@@ -6,4 +6,17 @@ $(document).ready(function () {
 			$(".loader_inner").fadeOut();
 		});	
 	});
+	
+	 $('#countdown').countDown({
+            targetDate: {
+                'day': 		31,
+                'month': 	12,
+                'year': 	2015,
+                'hour': 	0,
+                'min': 		0,
+                'sec': 		0
+            },
+            omitWeeks: true
+        });
+
 });

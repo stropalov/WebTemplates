3d2346370bbae1d9699b7293f3ed72383148ccec
stropalov <stropalov@gmail.com>
2015-05-23 21:05:43 +0300

    Fix blog section. colors

--- hostingTemplate/js/main.js
+++ hostingTemplate/js/main.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
 
 	$("#owl-example").owlCarousel({
 		autoPlay: 4000,
-		slideSpeed: 800,
+		slideSpeed: 1000,
 		navigation: false,
 		pagination: true,
 		singleItem: true,
